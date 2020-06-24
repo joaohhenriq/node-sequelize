8143248b1066e4458316ceeabfef6398bd0033de
joaoh <joao_camargo10@hotmail.com>
2020-06-24 15:25:10 -0300

    Creating db config

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: "postgres",
+    host: "localhost",
+    username: "postgres",
+    password: "123456",
+    database: "node_sequelize",
+    define: {
+        timestamps: true,
+        underscored: true,
+    }
+}
